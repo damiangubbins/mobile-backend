4818d83f79f8c30c7545744c73bb77a1ca88309d
damiangubbins <damiangubbins@uc.cl>
2025-05-07 11:54:48 -0400

    feat: add CORS support to responses in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::Request;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::response::Response;
+
 mod db;
 mod items;
 mod orders;
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .attach(items::stage())
         .attach(orders::stage())
 }
